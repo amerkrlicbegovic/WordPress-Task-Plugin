79009584f1e49ab357fd60719682246932e86f31
Amer <amer.krlicbegovic@gmail.com>
2018-07-06 19:55:57 +0200

    assign capabilites to roles

--- taskbook.php
+++ taskbook.php
@@ -29,3 +29,10 @@ along with Task Book. If not, see https://www.gnu.org/licenses/gpl.html.
  */
 require_once plugin_dir_path( __FILE__ ) . 'includes/posttypes.php';
 register_activation_hook( __FILE__, 'taskbook_rewrite_flush' );
+
+/**
+ * Register Task Logger role.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'includes/roles.php';
+register_activation_hook( __FILE__, 'taskbook_register_role' );
+register_deactivation_hook( __FILE__, 'taskbook_remove_role' );
